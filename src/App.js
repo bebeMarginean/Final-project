2ba7b3270598baa2cdc9307d8b7d0c4cbb72098a
Marginean Viorel <bebe.marginean@gmail.com>
2022-01-15 15:32:55 +0200

    add routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import Header from './Components/header/header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './Components/footer/footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Header/>
+     <BrowserRouter>
+     <Routes>
+       <Route path="/" element={<div>Homepage</div>}/>
+       <Route path="/about" element={<div>About</div>}/>
+       <Route path="/products" element={<div>Products</div>}/> 
+       <Route path=":productId" element={<div>Product Details</div>}/>
+     </Routes>
+     </BrowserRouter>
+     <Footer/>
+     </div>
+   );
 }
 
 export default App;
